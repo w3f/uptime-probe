71ff2395712be693ca6853d8bff622bbbb849118
Federico Gimenez <federico.gimenez@gmail.com>
2019-04-09 15:22:30 +0200

    first server version

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate prometheus;
+
+#[macro_use] extern crate serde_derive;
+
+#[macro_use] extern crate mime;
 
 use std::error::Error;
 pub use config::Config;
 
-mod config;
+//use checker;
 
+mod config;
+//mod checker;
+mod server;
+mod metrics;
 
 pub fn run(cfg: config::Config) -> Result<(), Box<dyn Error>> {
+    let metrics = metrics::Metrics::new();
+    //let checker = checker::Checker::new(cfg, metrics);
+    let _server = server::Server::new(cfg.port, &metrics)?;
 
     Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,44 @@
+extern crate iron;
+
+use std::error::Error;
+use iron::prelude::*;
+extern crate router;
+use router::Router;
+
+use iron::status;
+
+use crate::metrics;
+
+pub struct Server<'a>  {
+    metrics: &'a metrics::Metrics,
+}
+
+impl<'a> Server<'a> {
+    pub fn new(port: u32, metrics: &'a metrics::Metrics) -> Result<Server<'a>, Box<Error>> {
+        let mut router = Router::new();
+        router.get("/healthcheck", Self::get_healthcheck, "healthcheck");
+        router.get("/metrics", Self::get_metrics, "metrics");
+
+        println!("Serving on http://localhost:{}...", port);
+
+        Iron::new(router).http(format!("0.0.0.0:{}", port))?;
+
+        Ok(Server{ metrics })
+    }
+
+    fn get_healthcheck(_request: &mut Request) -> IronResult<Response> {
+        let mut response = Response::new();
+        response.set_mut(status::Ok);
+        response.set_mut(mime!(Text/Html; Charset=Utf8));
+        response.set_mut("Ok!");
+        Ok(response)
+    }
+
+    fn get_metrics(_request: &mut Request) -> IronResult<Response> {
+        let mut response = Response::new();
+        response.set_mut(status::Ok);
+        response.set_mut(mime!(Text/Html; Charset=Utf8));
+        response.set_mut("Ok!");
+        Ok(response)
+    }
+}
