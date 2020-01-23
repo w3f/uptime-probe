5e58a7318dcaac2c0085752ac90ebf257a07dd56
Federico Gimenez <federico.gimenez@gmail.com>
2020-01-23 16:55:03 +0100

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-/scripts/deploy.sh helm \
-                   --set image.tag="${CIRCLE_TAG}" \
-                   uptime-probe \
-                   w3f/uptime-probe
+/scripts/deploy.sh -t helm -a "--set image.tag=${CIRCLE_TAG} uptime-probe w3f/uptime-probe"
