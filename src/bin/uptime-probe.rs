f8d0129d672d081f768b731e94c911c77afde28a
Federico Gimenez <federico.gimenez@gmail.com>
2020-02-26 18:37:56 +0100

    use async/await

--- src/bin/uptime-probe.rs
+++ src/bin/uptime-probe.rs
@@ -2,7 +2,8 @@ use std::env;
 use std::process;
 use uptime_probe::{Config, run};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|e| {
@@ -10,8 +11,11 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
-        process::exit(1);
+    match run(config).await {
+        Err(e) => {
+            println!("Application error: {}", e);
+            process::exit(1);
+        }
+        Ok(_) => {}
     }
 }
