315aeb8c067ecc95289600ca02e162669fcae5f8
pantelis <pampatzoglou@gmail.com>
2020-05-25 13:10:36 +0300

    add origin to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/scripts/deploy.sh -t helm -a "--set image.tag=${CIRCLE_TAG} uptime-probe w3f/uptime-probe"
+/scripts/deploy.sh -t helm -a "--set image.tag=${CIRCLE_TAG} --set origin=${ORIGIN} uptime-probe w3f/uptime-probe"
